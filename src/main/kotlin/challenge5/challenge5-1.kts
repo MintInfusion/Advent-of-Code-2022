2e7ffe34c5a6f0724e12ae47b79dc929fac63504
Joel Koschier <joel.koschier@senacor.com>
2022-12-05 20:04:41 +0100

    Challenge 5

---
+++ src/main/kotlin/challenge5/challenge5-1.kts
@@ -0,0 +1,53 @@
+import java.io.File
+
+val (stackString, instructionString) = File("./input.txt")
+    .readText()
+    .split("\n\n")
+
+val instructions = instructionString.split("\n")
+    .map {
+        val (move, from, to) = Regex("move (\\d+) from (\\d+) to (\\d+)")
+            .matchEntire(it)!!
+            .destructured
+        Instruction(
+            Integer.parseInt(move),
+            Integer.parseInt(from),
+            Integer.parseInt(to))
+    }
+
+
+val numberOfStacks = stackString.split("\n").last().split("   ").map { Integer.parseInt(it.strip()) }.max()
+val stacks =  Array(numberOfStacks) { _ -> ArrayDeque<String>()}
+
+stackString
+    .split("\n")
+    .reversed()
+    .drop(1)
+    .forEach {
+        var toTake = it
+        var iter = 0
+        while(toTake != "") {
+            val token = toTake.take(3)
+            if (token != "   ")
+                stacks[iter].addLast(token.subSequence(1, 2).toString())
+            toTake = toTake.drop(4)
+            iter++
+        }
+    }
+
+instructions.forEach {
+    repeat(it.move) { _ ->
+        val token = stacks[it.from - 1].removeLast()
+        stacks[it.to - 1].addLast(token)
+    }
+}
+
+stacks.forEach {
+    print(it.last())
+}
+
+data class Instruction(
+    val move: Int,
+    val from: Int,
+    val to: Int,
+)
